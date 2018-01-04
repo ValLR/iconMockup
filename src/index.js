46721bc1c0a9338ebdba8b7cd4d1f576b4f79ffb
ValLR <valentinalopezrecabal@gmail.com>
2018-01-04 11:15:36 -0300

    modificando componenetes para incluir imágenes en lugar de fonawesome

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ class Icon extends React.Component {
 	constructor(props){
 		super(props)
 		this.state = {
+			service:"",
 			url1:"",
 			url2:"",
 			user1:"",
@@ -15,14 +16,16 @@ class Icon extends React.Component {
 	}
 
 	componentDidMount(){
-		this.setState({url1:null,url2:"github.com", user1:true, user2:false});
+		this.setState({service:"github",url1:null,url2:"https://github.com/", user1:true, user2:false});
 	}
 	render(){
 		return (
-			<div>
-				<Logo url= {this.state.url1} userAccess= {this.state.user2}/>
-				<Logo url= {this.state.url2} userAccess= {this.state.user1}/>
-				<Logo url= {this.state.url2} userAccess= {this.state.user2}/>
+			<div class="container">
+				<div class="row">
+					<Logo service={this.state.service} url= {this.state.url1} userAccess= {this.state.user2}/>
+					<Logo service={this.state.service} url= {this.state.url2} userAccess= {this.state.user1}/>
+					<Logo service={this.state.service} url= {this.state.url2} userAccess= {this.state.user2}/>
+				</div>
 			</div>
 		)
 	}
@@ -33,34 +36,52 @@ class Icon extends React.Component {
 function Logo(props) {
 	//const opciones = (this.props.url === "") ? console.log("grey no access") : console.log("normal")
 	if(props.url !== null && props.userAccess !== false){
-		return <Normal />	
+		return <Normal service={props.service} href={props.url}/>	
 	}
 	else if(props.url === null){
-		return <NoProject />
+		return <NoProject service={props.service} />
 	}
 	else {
-		return <NoAccess />
+		return <NoAccess service={props.service} href={props.url}/>
 	}
 
 }
+//this.props.service === github --->icon github
 
-//noaccess
-//nouser
-//allgreat
 class NoProject extends React.Component {
   render(){
-  	return <a>noProject</a>
+  	return (
+  		<div class="col-lg-4 col-mg-4 text-center">
+  			<a href="#" class="disabled">
+  				<i class="fa fa-github fa-5x" aria-hidden="true"></i>
+  			</a>
+  		</div>
+  	);
   }
 
 }
 class NoAccess extends React.Component {
 	render(){
-		return <a>noAccess</a>
+	  	return (
+	  		<div class="col-lg-4 col-mg-4 text-center">
+		  		<a href={this.props.href} class="noAccess">
+		  			<i class="fa fa-github fa-5x" aria-hidden="true"></i>
+		  			<br />
+	  				<i class="fa fa-minus-circle" aria-hidden="true"></i>	  			
+		  		</a>	  			
+	  		</div>
+	  	);		
 	}
 }
 class Normal extends React.Component {
 	render(){
-		return <a>Normal</a>
+	  	return (
+	  		<div class="col-lg-4 col-mg-4 text-center">
+	  			<a href={this.props.href} class="normal">
+	  				<i class="fa fa-github fa-5x" aria-hidden="true"></i>
+	  			</a>
+	  		</div>
+	  	);		
 	}
 }
 
