d2404fe8c1518fe48d7331843ffcc470a4765e03
ValLR <valentinalopezrecabal@gmail.com>
2018-01-04 15:28:57 -0300

    cambiando prueba desde state a json

--- src/index.js
+++ src/index.js
@@ -7,7 +7,11 @@ class Icon extends React.Component {
 	constructor(props){
 		super(props)
 		this.state = {
-			service:"",
+			service1:"",
+			service2:"",
+			service3:"",
+			service4:"",
+			service5:"",
 			url1:"",
 			url2:"",
 			user1:"",
@@ -16,15 +20,45 @@ class Icon extends React.Component {
 	}
 
 	componentDidMount(){
-		this.setState({service:"github",url1:null,url2:"https://github.com/", user1:true, user2:false});
+		this.setState({
+			service1:"github",
+			service2:"pivotal",
+			service3:"codeship",
+			service4:"papertrail",
+			service5:"sentry",
+			url1:null,
+			url2:"https://github.com/",
+			user1:true,
+			user2:false
+		});
 	}
 	render(){
 		return (
 			<div class="container">
 				<div class="row">
-					<Logo service={this.state.service} url= {this.state.url1} userAccess= {this.state.user2}/>
-					<Logo service={this.state.service} url= {this.state.url2} userAccess= {this.state.user1}/>
-					<Logo service={this.state.service} url= {this.state.url2} userAccess= {this.state.user2}/>
+					<Logo service={this.state.service1} url= {this.state.url1} userAccess= {this.state.user2}/>
+					<Logo service={this.state.service1} url= {this.state.url2} userAccess= {this.state.user1}/>
+					<Logo service={this.state.service1} url= {this.state.url2} userAccess= {this.state.user2}/>
+				</div>
+				<div class="row">
+					<Logo service={this.state.service2} url= {this.state.url1} userAccess= {this.state.user2}/>
+					<Logo service={this.state.service2} url= {this.state.url2} userAccess= {this.state.user1}/>
+					<Logo service={this.state.service2} url= {this.state.url2} userAccess= {this.state.user2}/>
+				</div>
+				<div class="row">
+					<Logo service={this.state.service3} url= {this.state.url1} userAccess= {this.state.user2}/>
+					<Logo service={this.state.service3} url= {this.state.url2} userAccess= {this.state.user1}/>
+					<Logo service={this.state.service3} url= {this.state.url2} userAccess= {this.state.user2}/>
+				</div>
+				<div class="row">
+					<Logo service={this.state.service4} url= {this.state.url1} userAccess= {this.state.user2}/>
+					<Logo service={this.state.service4} url= {this.state.url2} userAccess= {this.state.user1}/>
+					<Logo service={this.state.service4} url= {this.state.url2} userAccess= {this.state.user2}/>
+				</div>
+				<div class="row">
+					<Logo service={this.state.service5} url= {this.state.url1} userAccess= {this.state.user2}/>
+					<Logo service={this.state.service5} url= {this.state.url2} userAccess= {this.state.user1}/>
+					<Logo service={this.state.service5} url= {this.state.url2} userAccess= {this.state.user2}/>
 				</div>
 			</div>
 		)
@@ -47,13 +81,28 @@ function Logo(props) {
 
 }
 //this.props.service === github --->icon github
+//como concatenar {props.service} en medio del path
+
+class Favicon extends React.Component {
+	render(){
+		const service = this.props.service;
+		const initialPath = "/images/";
+		const finalPath = ".png";
+		const source = initialPath + service + finalPath;
+		console.log(source);
+
+		return <img class={this.props.style} src={source} />		
+	}
+}
+
 
 class NoProject extends React.Component {
   render(){
   	return (
+
   		<div class="col-lg-4 col-mg-4 text-center">
   			<a href="#" class="disabled">
-  				<i class="fa fa-github fa-5x" aria-hidden="true"></i>
+  				<Favicon service={this.props.service} style="disabled-icon"/>
   			</a>
   		</div>
   	);
@@ -65,7 +114,7 @@ class NoAccess extends React.Component {
 	  	return (
 	  		<div class="col-lg-4 col-mg-4 text-center">
 		  		<a href={this.props.href} class="noAccess">
-		  			<i class="fa fa-github fa-5x" aria-hidden="true"></i>
+					<Favicon service={this.props.service} style="no-access"/>
 		  			<br />
 	  				<i class="fa fa-minus-circle" aria-hidden="true"></i>	  			
 		  		</a>	  			
@@ -78,7 +127,7 @@ class Normal extends React.Component {
 	  	return (
 	  		<div class="col-lg-4 col-mg-4 text-center">
 	  			<a href={this.props.href} class="normal">
-	  				<i class="fa fa-github fa-5x" aria-hidden="true"></i>
+					<Favicon service={this.props.service} style="full-access"/>
 	  			</a>
 	  		</div>
 	  	);		
